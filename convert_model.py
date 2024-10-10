db1f469179e2821332802bac2e1f8932f4879f95
Pon <pon.m22@gmail.com>
2024-10-10 14:47:18 +0700

    :hammer: Update Convert model code

--- convert_model.py
+++ convert_model.py
@@ -8,7 +8,7 @@ class CustomDepthwiseConv2D(DepthwiseConv2D):
         super().__init__(**kwargs)
         self.groups = groups
 
-model = tf.keras.models.load_model('model/new_model/keras_model.h5', custom_objects={'DepthwiseConv2D': CustomDepthwiseConv2D})
+model = tf.keras.models.load_model('/Users/ponmorin/Documents/Python_MachineLearning/Real_Bondro_Objectdetection/model/bondro_mark3/keras_model.h5', custom_objects={'DepthwiseConv2D': CustomDepthwiseConv2D})
 converter = tf.lite.TFLiteConverter.from_keras_model(model)
 tflite_model = converter.convert()
-open("model/new_model/bottle_model2.tflite", "wb").write(tflite_model)
+open("/Users/ponmorin/Documents/Python_MachineLearning/Real_Bondro_Objectdetection/model/bondro_mark3/bondro_modelV3", "wb").write(tflite_model)
